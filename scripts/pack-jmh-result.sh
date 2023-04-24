dc35ef704d6cc0be61d6be3f6869db8470025b57
James Bellenger <james.bellenger@airbnb.com>
2023-04-24 16:56:49 -0700

    expand each result into a BQ record

--- scripts/pack-jmh-result.sh
+++ scripts/pack-jmh-result.sh
@@ -9,23 +9,23 @@ if [ ! -e "$RESULTS_FILE" -o ! -f "$RESULTS_FILE" ]; then
   exit 1
 fi
 
-OUTPUT=$(dirname "$RESULTS_FILE")/row.json
+OUTPUT=$(dirname "$RESULTS_FILE")/rows.json
 
 JSON=$(cat $RESULTS_FILE | jq -c '.[0]')
 GIT_SHA=$(git rev-parse HEAD)
 GIT_BRANCH=$(git branch --show-current)
 STAMP=$(date -u +"%Y-%m-%d %H:%M:%S UTC")
 
+# BigQuery expects json data to be objects
+# This is a challenge when working with jmh result data, which formats results
+# as an array of individual benchmark results.
+# We can work around this by inserting a row for each benchmark result in that
+# array.
 
-DOC=$(cat <<EOT
-{
-  "gitsha": "${GIT_SHA}",
-  "branch": "${GIT_BRANCH}",
-  "jmh_json": ${JSON},
-  "stamp": "${STAMP}"
-}
-EOT
-)
+JQ_RUN_TEMPLATE="{git_sha: \"${GIT_SHA}\", git_branch: \"${GIT_BRANCH}\", stamp: \"${STAMP}\", jmh_json: .}"
 
-echo $DOC | jq -c > $OUTPUT
+# For each row in the jmh result array, format it with some metadata from its
+# test run and emit it as a line in a file.
+cat $RESULTS_FILE | jq -c ".[] | $JQ_RUN_TEMPLATE" > $OUTPUT
+
 echo $OUTPUT
